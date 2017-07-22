b77c88c5d713282c5fc3ee4c487ea12d985b9b34
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-22 14:20:08 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,17 +1,9 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  
-  var result=[];
-  var collection=[];
-  if(collectionA.length>collectionB.length){
-      collection=collectionA;
-      collectionA=collectionB;
-      collectionB=collection;
-  }
-  for(i=0;i<collectionA.length;i++)
-      if(collectionB.indexOf(collectionA[i])>-1)
-         result.push(collectionA[i]) 
-  return result;
-  
+    var result=[];
+    for(let i=0;i<collectionA.length;i++)
+        if(collectionB.indexOf(collectionA[i])>-1)
+            result.push(collectionA[i]);
+    return result; 
 }
