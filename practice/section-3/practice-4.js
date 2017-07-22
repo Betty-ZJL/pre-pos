c240a224d3c54421ed7544adb5908f5b5060a3db
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-22 15:48:34 +0800

    Update practice-4.js

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,36 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var collectionC=[];
+  while(collectionA.length){
+      var flag=1;
+      var x=collectionA.shift();
+      if(x.length>1){
+          var n=parseInt(x.substring(x.length-1));
+          x=x.substring(0,1);
+          flag=2;
+      }
+      for(let i=0;i<collectionC.length;i++){
+          if(collectionC[i].key==x){
+              collectionC[i].count++;
+              flag=0;
+          }
+      }
+      if(flag==1){
+          collectionC.push({key:x,count:1});
+      }
+      if(flag==2){
+          collectionC.push({key:x,count:n});
+      }
+  }
+  for(let i=0;i<collectionC.length;i++){
+      if(objectB.value.indexOf(collectionC[i].key)>-1){
+          let n=collectionC[i].count;
+          while(n>=3){
+              collectionC[i].count--; 
+              n-=3;
+          }
+      }          
+  }
+  return collectionC;
 }
