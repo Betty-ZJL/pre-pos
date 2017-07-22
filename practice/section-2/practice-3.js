faabfa94c721e3c32637062c8112a71582e4dea5
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-22 15:17:48 +0800

    Update practice-3.js

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,35 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+   var result=[];
+   while(collection.length){
+      var flag=1;
+      var x=collection.shift();
+      if(x.length>1){
+          if(x.length>4){
+               var n=parseInt(x.substring(2,4));
+          }else{
+               var n=parseInt(x.substring(2,3));
+          }
+          x=x.substring(0,1);
+          flag=2;
+      }
+      for(let i=0;i<result.length;i++){
+          if(result[i].name==x){
+              if(flag==2){
+                  result[i].summary+=n;
+              }else{
+                  result[i].summary++;
+              }
+              flag=0;
+          }
+      }
+      if(flag==1){
+          result.push({name:x,summary:1});
+      }
+      if(flag==2){
+          result.push({name:x,summary:n});
+      }
+  }
+  return result;
 }
